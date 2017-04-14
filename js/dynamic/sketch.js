80bcbec32b4daa733979eb38b44239d0648504d3
juanka588 <jcrodriguezd@unal.edu.co>
2017-04-13 23:42:08 -0500

    TODO: movement over sphere

--- js/dynamic/sketch.js
+++ js/dynamic/sketch.js
@@ -1,18 +1,48 @@
 var controls = new Controls();
 
-var gray = 0;
+var spider;
+var batman;
+var img;
+var world;
+var destination;
+var velocity;
+
 function setup() {
-    var canvas = createCanvas(600, 600);
+    var canvas = createCanvas(600, 600, WEBGL);
     canvas.parent('sketch-holder');
+    spider = new Spider(0, 0, 0, 100);
+//    batman = loadModel('assets/spider/Only_Spider_with_Animations_Export.obj');
+//    batman = loadModel("assets/Batman/BatmanArmoured.obj");
+//    img = loadImage("assets/spider/textures/Spinnen_Bein_tex.jpg");
+    img = loadImage("images/cur-texture.jpg");
+    world = loadImage("images/grass.jpg");
 }
 function draw() {
-    background(gray);
+    background("gray");
     push();
+    translate(-spider.x, -spider.y, 0);
+    specularMaterial("red");
     controls.controle();
-    rect(width / 2, height / 2, 200, 200);
+    push();
+    texture(img);
+    spider.draw();
+    spider.move(destination, velocity);
+    pop();
+    push();
+    translate(0, 0, -width * 2 - spider.size);
+    texture(world);
+    sphere(width * 2);
+//    model(batman);
     pop();
 }
-
-function mousePressed() {
-    gray = (gray + 16) % 256;
+function keyPressed() {
+    if (key === "W") {
+        if (spider.isMoving) {
+            spider.isMoving = false;
+        } else {
+            spider.isMoving = true;
+            destination = {x: spider.x, y: spider.y - spider.size};
+            velocity = {x: 0, y: -3};
+        }
+    }
 }
--- js/dynamic/spider.js
+++ js/dynamic/spider.js
@@ -1,7 +1,119 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function Spider(x, y, z, size) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.size = size;
+    this.dir = 1;
+    this.speed = 0.9;
+    this.isMoving = false;
+    this.draw = function () {
+        push();
+        translate(this.x, this.y, this.z);
+        this.drawBody();
+        if (this.isMoving) {
+            var rotation = frameCount % 40;
+            if (rotation === 0) {
+                this.dir = -this.dir;
+            }
+            if (this.dir === -1) {
+                rotation = 40 - rotation;
+            }
+        }
+        translate(0, this.size / 3, 0);
+        push();
+        if (this.isMoving) {
+            rotateZ(PI * -40 / 180);
+            rotateZ(this.speed * PI * rotation / 180);
+        }
+        this.drawLeg(1);
+        this.drawLeg(2);
+        this.drawLeg(3);
+        this.drawLeg(4);
+        pop();
+        push();
+        if (this.isMoving) {
+            rotateZ(this.speed * PI * rotation / 180);
+        }
+        this.drawLeg(5);
+        this.drawLeg(6);
+        this.drawLeg(7);
+        this.drawLeg(8);
+        pop();
+        pop();
+    };
 
+    this.move = function (destination, vel) {
+        if (this.isMoving) {
+            this.x += vel.x;
+            this.y += vel.y;
+            if (dist(this.x, destination.x) <= vel.x + 10
+                    && dist(this.y, destination.y) <= vel.y + 10) {
+                this.isMoving = false;
+            }
+        }
+    };
 
+    this.drawBody = function () {
+        push();
+        sphere(this.size);
+        translate(0, -120, 0);
+        sphere(this.size - 30);
+        pop();
+    };
+
+    this.drawLeg = function (n) {
+        push();
+        switch (n) {
+            case 1:
+                translate(-this.size, -this.size, 0);
+                rotateZ(PI / 4);
+                this.drawLine(this.size / 4, 1);
+                break;
+            case 2:
+                translate(-this.size, this.size, 0);
+                rotateZ(PI / 4);
+                this.drawLine(this.size / 4, 1);
+                break;
+            case 3:
+                translate(-this.size, -this.size / 4, 0);
+                rotateZ(PI / 4);
+                this.drawLine(this.size / 4, 1);
+                break;
+            case 4:
+                translate(-this.size, 3 * this.size / 8, 0);
+                rotateZ(PI / 4);
+                this.drawLine(this.size / 4, 1);
+                break;
+            case 5:
+                translate(this.size, 3 * this.size / 8, 0);
+                rotateZ(-PI / 4);
+                this.drawLine(this.size / 4, -1);
+                break;
+            case 6:
+                translate(this.size, -this.size / 4, 0);
+                rotateZ(-PI / 4);
+                this.drawLine(this.size / 4, -1);
+                break;
+            case 7:
+                translate(this.size, this.size, 0);
+                rotateZ(-PI / 4);
+                this.drawLine(this.size / 4, -1);
+                break;
+            case 8:
+                translate(this.size, -this.size, 0);
+                rotateZ(-PI / 4);
+                this.drawLine(this.size / 4, -1);
+                break;
+        }
+        pop();
+    };
+
+    this.drawLine = function (s, dir) {
+        if (s >= 1) {
+            sphere(s);
+            translate(0, -s, -s);
+            rotateZ(dir * PI / 7);
+            this.drawLine(s - 3, dir);
+        }
+    };
+}
