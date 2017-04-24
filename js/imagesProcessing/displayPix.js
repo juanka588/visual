1c335b61ff3b2b9a052dd792be299eec9871fbc2
juanka588 <jcrodriguezd@unal.edu.co>
2017-04-23 20:30:03 -0500

    add 3d support in a weird form

--- js/imagesProcessing/displayPix.js
+++ js/imagesProcessing/displayPix.js
@@ -4,10 +4,21 @@ function DisplayPixel(x, y, z, r, c) {
     this.z = z;
     this.r = r;
     this.color = c;
-    this.draw = function () {
-        noStroke();
-        fill(this.color);
-        rect(x, y, this.r, this.r);
+    this.draw = function (p) {
+        if (arguments.length > 0) {
+            p.push();
+            p.translate(this.x, this.y, this.z);
+            p.noStroke();
+            p.fill(this.color);
+            p.rect(0, 0, this.r, this.r);
+            p.pop();
+        } else {
+            push();
+            translate(this.x, this.y, this.z);
+            specularMaterial(this.color);
+            box(this.r);
+            pop();
+        }
     };
 }
 
--- js/imagesProcessing/main.js
+++ js/imagesProcessing/main.js
@@ -1,44 +1,73 @@
 var controls = new Controls();
 var originalImg;
 var img;
-var details = 4;
+var details = 2;
 var displayUnits = [];
+var canvasWidth = 600;
 
 
-function preload() {
-    originalImg = loadImage("images/mario.jpg");
-}
+new p5(function (p) {
+    p.preload = function () {
+        originalImg = p.loadImage("images/mario.jpg");
+    };
 
-function setup() {
-    var canvas = createCanvas(600, 600);
-    canvas.parent('sketch-holder');
-    image(originalImg, 0, 0);
-    loadPixels();
-    var h = originalImg.height / details;
-    var w = originalImg.width / details;
-    var d = pixelDensity();
-    var idx = 0;
+    p.setup = function () {
+        p.createCanvas(600, 500);
+        p.loadImageSource();
+    };
+    p.draw = function () {
+        p.background(255);
+//        p.push();
+//        for (var i = 0; i < displayUnits.length; i++) {
+//            displayUnits[i].draw(p);
+//        }
+//        p.pop();
+    };
 
-    for (var x = 0; x < w; x++) {
-        for (var y = 0; y < h; y++) {
-            for (var i = 0; i < d; i++) {
-                for (var j = 0; j < d; j++) {
-                    idx = 4 * details * ((y * d + j) * width * d + (x * d + i));
-                    var r = pixels[idx];
-                    var g = pixels[idx + 1];
-                    var b = pixels[idx + 2];
-                    var a = pixels[idx + 3];
-                    var clr = color(r, g, b, a);
-                    displayUnits.push(new DisplayPixel(x * details, y * details, 0, details, clr));
+    p.loadImageSource = function () {
+        p.image(originalImg, 0, 0);
+        p.loadPixels();
+        var h = originalImg.height / details;
+        var w = originalImg.width / details;
+        var d = p.pixelDensity();
+        var idx = 0;
+
+        for (var x = 0; x < w; x++) {
+            for (var y = 0; y < h; y++) {
+                for (var i = 0; i < d; i++) {
+                    for (var j = 0; j < d; j++) {
+                        idx = 4 * details * ((y * d + j) * canvasWidth * d + (x * d + i));
+                        var r = p.pixels[idx];
+                        var g = p.pixels[idx + 1];
+                        var b = p.pixels[idx + 2];
+                        var a = p.pixels[idx + 3];
+                        var clr = p.color(r, g, b, a);
+                        displayUnits.push(new DisplayPixel(x * details, y * details, hue(clr), details, clr));
+                    }
                 }
             }
         }
-    }
+        pixels = null;
+        originalImg = null;
+
+    };
+
+
+});
+
+
+function setup() {
+    var canvas = createCanvas(canvasWidth, 600, WEBGL);
+    canvas.parent('sketch-holder');
 }
 
 function draw() {
     background(125);
+    push();
+    translate(-width / 2, -height / 4, 0);
+    controls.controle();
     for (var i = 0; i < displayUnits.length; i++) {
         displayUnits[i].draw();
     }
+    pop();
 }
